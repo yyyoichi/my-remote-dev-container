579c77441cd3e68d948bb2e69b4568a7aabd3ac4
yyyoichi <origamiichiro@gmail.com>
2025-05-09 15:54:16 +0900

    とりあえずインスタンス作成

--- bin/development-instance-cdk.ts
+++ bin/development-instance-cdk.ts
@@ -4,17 +4,5 @@ import { DevelopmentInstanceCdkStack } from '../lib/development-instance-cdk-sta
 
 const app = new cdk.App();
 new DevelopmentInstanceCdkStack(app, 'DevelopmentInstanceCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {region: process.env.CDK_DEFAULT_REGION, account: process.env.CDK_DEFAULT_ACCOUNT}
 });
--- lib/development-instance-cdk-stack.ts
+++ lib/development-instance-cdk-stack.ts
@@ -1,16 +1,116 @@
-import * as cdk from 'aws-cdk-lib';
+import {
+  Stack,
+  StackProps,
+  aws_ec2 as ec2,
+  aws_iam as iam,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class DevelopmentInstanceCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface DevelopmentInstanceCdkStackProps extends StackProps {
+}
+
+export class DevelopmentInstanceCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props: DevelopmentInstanceCdkStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 既存のVPCを参照
+    const vpc = new ec2.Vpc(this, 'DevelpmentIncetanceVpc', {
+      maxAzs: 1,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'private-subnet',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DevelopmentInstanceCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // NATインスタンス用セキュリティグループ
+    const natSg = new ec2.SecurityGroup(this, 'NatInstanceSG', {
+      vpc,
+      description: 'Security group for NAT instance',
+      allowAllOutbound: true,
+    });
+
+    natSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
+    natSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
+
+    // NAT用のロール
+    const natRole = new iam.Role(this, 'NatInstanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2ReadOnlyAccess'),
+      ],
+    });
+
+    // NAT用AMI
+    const natAmi = ec2.MachineImage.latestAmazonLinux2023();
+
+    // NATインスタンス
+    const natInstance = new ec2.Instance(this, 'NatInstance', {
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.NANO),
+      machineImage: natAmi,
+      securityGroup: natSg,
+      role: natRole,
+      sourceDestCheck: false, // NATには無効化が必要
+    });
+
+    // Debian EC2インスタンスのセキュリティグループ
+    const ec2Sg = new ec2.SecurityGroup(this, 'Ec2InstanceSG', {
+      vpc,
+      allowAllOutbound: true,
+    });
+    ec2Sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH from anywhere');
+
+    // Debian用AMI
+    const debianAmi = ec2.MachineImage.genericLinux({
+      [props.env?.region!]: 'ami-00a7d6f3b78d70c5a',
+    });
+
+    // Debian EC2インスタンス（Dockerインストール前提）
+    const instance = new ec2.Instance(this, 'DebianEc2ContainerHost', {
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      machineImage: debianAmi,
+      securityGroup: ec2Sg,
+      keyPair: new ec2.KeyPair(this, 'DevelopmentKey'),
+      blockDevices: [
+        {
+          deviceName: '/dev/xvda',
+          volume: ec2.BlockDeviceVolume.ebs(20),
+        },
+      ],
+    });
+
+    // ルートテーブルの更新（Private SubnetからNATへ）
+    vpc.privateSubnets.forEach((subnet, index) => {
+      // 新しいルートテーブルを作成
+      const routeTable = new ec2.CfnRouteTable(this, `PrivateRouteTable-${index}`, {
+        vpcId: vpc.vpcId,
+      });
+
+      // サブネットとルートテーブルを関連付け
+      new ec2.CfnSubnetRouteTableAssociation(this, `PrivateSubnetAssoc-${index}`, {
+        subnetId: subnet.subnetId,
+        routeTableId: routeTable.ref,
+      });
+
+      // ルートテーブルにNATインスタンスへのルートを追加
+      new ec2.CfnRoute(this, `PrivateRoute-${index}`, {
+        routeTableId: routeTable.ref,
+        destinationCidrBlock: '0.0.0.0/0',
+        instanceId: natInstance.instanceId,
+      });
+    });
   }
 }
